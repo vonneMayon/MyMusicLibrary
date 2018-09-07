5c670456e6711a5c26fab6effbaab118ef7e5809
Maureen Kariuki <kmaureen44@gmail.com>
2018-09-07 00:22:31 -0700

    uncommented out LibraryHelper methods

--- MyMusicLibrary/MyMusicLibrary/LibraryHelper.cs
+++ MyMusicLibrary/MyMusicLibrary/LibraryHelper.cs
@@ -8,35 +8,35 @@ namespace MyMusicLibrary
 {
     public static class LibraryHelper
     {
-        //   public static async void ChooseMusicAsync()
-        //{
-        //    //Music Library is opened on user's computer and displays all available mp3 files
-        //    var picker = new Windows.Storage.Pickers.FileOpenPicker();
-        //    picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
-        //    picker.SuggestedStartLocation =
-        //    Windows.Storage.Pickers.PickerLocationId.MusicLibrary;
-        //    picker.FileTypeFilter.Add(".mp3");
-        //    picker.PickSingleFileAsync();
-        //    //(TO DO)
-        //    //need to return file path to reference when storing music?
-        //}
-        
-        //public static async void StoreMusic()
-        
-        //{
-        ////(TO DO)
-        ////create folder accessible to app
-        ////system checks if folder is already there and doesn't recreate folder if it already exists
-        ////move mp3 file chosen from filepicker into folder
-        
-        //}
-        
-        //public static async void GetID3Tags()
-        //{
-        //// (TO DO)
-        ////definition: function to extract ID3 tags: Title, Artist, Album Name from mp3 file
-        ////return Title, Artist, Album Name to be used on page displaying all stored music
-        //}
+        public static async void ChooseMusicAsync()
+        {
+            //Music Library is opened on user's computer and displays all available mp3 files
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation =
+            Windows.Storage.Pickers.PickerLocationId.MusicLibrary;
+            picker.FileTypeFilter.Add(".mp3");
+            picker.PickSingleFileAsync();
+            //(TO DO)
+            //need to return file path to reference when storing music?
+        }
+
+        public static async void StoreMusic()
+
+        {
+            //(TO DO)
+            //create folder accessible to app
+            //system checks if folder is already there and doesn't recreate folder if it already exists
+            //move mp3 file chosen from filepicker into folder
+
+        }
+
+        public static async void GetID3Tags()
+        {
+            // (TO DO)
+            //definition: function to extract ID3 tags: Title, Artist, Album Name from mp3 file
+            //return Title, Artist, Album Name to be used on page displaying all stored music
+        }
 
     }
 }
