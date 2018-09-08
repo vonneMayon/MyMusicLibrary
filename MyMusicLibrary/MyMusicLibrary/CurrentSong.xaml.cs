743b7813f774a65ed036d2d73bccf5485fff92d4
Desireé Mayon <34898589+vonneMayon@users.noreply.github.com>
2018-09-07 19:25:36 -0700

    Add  CurrentSong

--- MyMusicLibrary/MyMusicLibrary/CurrentSong.xaml.cs
+++ MyMusicLibrary/MyMusicLibrary/CurrentSong.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -28,6 +29,32 @@ namespace MyMusicLibrary
             Song song = new Song("In Those Jeans", "Ginuwine");
             this.DataContext = song;
         }
-      
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await SetLocalMedia();
+        }
+
+        async private System.Threading.Tasks.Task SetLocalMedia()
+        {
+            var openPicker = new Windows.Storage.Pickers.FileOpenPicker();
+
+            openPicker.FileTypeFilter.Add(".wmv");
+            openPicker.FileTypeFilter.Add(".mp4");
+            openPicker.FileTypeFilter.Add(".wma");
+            openPicker.FileTypeFilter.Add(".mp3");
+
+            var file = await openPicker.PickSingleFileAsync();
+
+            // mediaPlayer is a MediaPlayerElement defined in XAML
+            if (file != null)
+            {
+                mediaPlayer.Source = MediaSource.CreateFromStorageFile(file);
+
+                mediaPlayer.MediaPlayer.Play();
+            }
+
+
+        }
+
     }
 }
