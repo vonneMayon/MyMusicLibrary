ed61959fdac882a37dd689f75eaca2ec21f5c28f
Desireé Mayon <34898589+vonneMayon@users.noreply.github.com>
2018-09-07 17:19:34 -0700

    Add Song class

---
+++ MyMusicLibrary/MyMusicLibrary/Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMusicLibrary
+{
+    class Song
+    {
+        public string Title;
+        public string Artist;
+
+        public Song(string title, string artist)
+        {
+            Title = title;
+            Artist = artist;
+        }
+    }
+}
